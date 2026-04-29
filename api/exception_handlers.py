4e87628c47cc083c56df94ec2994fb6fc7f3721e
gianpaolo <ingrassiagianpaolo@gmail.com>
2026-04-29 06:52:11 +0200

    piccola modifica

--- api/exception_handlers.py
+++ api/exception_handlers.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
